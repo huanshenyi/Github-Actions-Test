067d72fba2b27ecffc5ec71eae662ba771277de6
Syoitu Den <42715311+huanshenyi@users.noreply.github.com>
2019-09-23 17:57:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,14 @@ from selenium import webdriver
 import time
 import os
 
-path = "./Selenium.WebDriver.IEDriver.3.150.0/driver"
+# path = process.env.PATH + "./Selenium.WebDriver.IEDriver.3.150.0/driver"
 
 
-driver = webdriver.Ie(executable_path=path)
-driver.get('https://bell-face.co/')
-con_btn = driver.find_element_by_partial_link_text('（資料ダウンロード）')
-con_btn.click()
-time.sleep(2)
-driver.close()
-driver.quit()
+# driver = webdriver.Ie(executable_path=path)
+# driver.get('https://bell-face.co/')
+# con_btn = driver.find_element_by_partial_link_text('（資料ダウンロード）')
+# con_btn.click()
+# time.sleep(2)
+# driver.close()
+# driver.quit()
+print(process.env.PATH)
