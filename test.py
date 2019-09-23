5f6b029fd462c6246d0cd6d3ed5a97c981772d24
Syoitu Den <42715311+huanshenyi@users.noreply.github.com>
2019-09-23 17:29:56 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello world")
+from selenium import webdriver
+import time
+import os
+
+path = "./Selenium.WebDriver.IEDriver.3.150.0/driver"
+
+
+driver = webdriver.Chrome(executable_path=path)
+driver.get('https://bell-face.co/')
+con_btn = driver.find_element_by_partial_link_text('（資料ダウンロード）')
+con_btn.click()
+time.sleep(2)
+driver.close()
+driver.quit()
