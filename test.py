88c02d84cc11289b3fe98e18794bb6b4b55f67f9
Syoitu Den <42715311+huanshenyi@users.noreply.github.com>
2019-09-27 16:41:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,11 +6,12 @@ import sys
 process.env.PATH = f"{process.env.PATH};{os.getcwd()}/Selenium.WebDriver.IEDriver.3.150.0/driver"
 #path =  "D:\a\Github-Actions-Test\Github-Actions-Test\Selenium.WebDriver.IEDriver.3.150.0\driver"
 
-driver = webdriver.Ie(executable_path=process.env.PATH)
+driver = webdriver.Ie(executable_path="Selenium.WebDriver.IEDriver.3.150.0\driver\IEDriverServer.exe")
 driver.get('https://bell-face.co/')
-con_btn = driver.find_element_by_partial_link_text('（資料ダウンロード）')
-con_btn.click()
+# con_btn = driver.find_element_by_partial_link_text('（資料ダウンロード）')
+# con_btn.click()
 time.sleep(2)
+print(driver.title)
 driver.close()
 driver.quit()
 
